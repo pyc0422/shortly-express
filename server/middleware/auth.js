d6b25e07654a871c222fc64f3db8d5a794122c6d
Connor Thurston <connor.thurston1@gmail.com>
2022-08-16 22:02:59 -0600

    end of tuesday night - createSession middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,28 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  // console.log('session cookies: ', req.cookies);
+
+  if (JSON.stringify(req.cookies) === '{}') {
+    return models.Sessions.create()
+      .then((data)=> {
+        return models.Sessions.get({id: data.insertId})
+          .then((hashData) => {
+            console.log('hashData: ', hashData.hash);
+            if (!req.session) {
+              req.session = {};
+              req.session.hash = hashData.hash;
+              res.cookie('shortlyid', hashData.hash);
+              // console.log( 'has cookies');
+              console.log('response cookies ', res.cookies);
+              return next();
+            }
+          });
+      });
+  }
+  console.log('next test');
+
+  next();
 };
 
 /************************************************************/
